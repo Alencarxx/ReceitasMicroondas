f1b1d2cf9ff2699ce0305f4c48c952b4ad868572
Alencar Moreira Gonçalves Porto <alencarmoreira@snews.tv>
2017-06-13 15:36:53 -0300

    Adicionar arquivos de projeto.

---
+++ ReceitasdMicroondas/ReceitasdMicroondas/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace ReceitasdMicroondas.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
